e99f04839fe3dfa21c407279140540479b77d680
Marius <marius@twostairs.com>
2016-08-31 21:47:13 +0200

    Removed Model

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-export { default as Model } from './Model';
-
 export { default as ApiController } from './ApiController';
 export { default as ApiModel } from './ApiModel';
 export { default as ApiRouter } from './ApiRouter';
