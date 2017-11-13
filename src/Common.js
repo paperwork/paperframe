52ace77a134c98fa05c7f3ceea1ac875c858e1bd
Marius <marius@twostairs.com>
2017-11-13 15:48:07 +0100

    Added RS_NOT_OK & RS_ERROR_GENERIC

--- src/Common.js
+++ src/Common.js
@@ -8,6 +8,8 @@ module.exports.FIRST = 0;
 
 /* Status Codes */
 module.exports.RS_OK = 0;
+module.exports.RS_NOT_OK = 1;
+
 module.exports.RS_TOKEN_INVALID = 10;
 module.exports.RS_AUTHENTICATION_FAILED = 20;
 module.exports.RS_AUTHENTICATION_SUCCEEDED = 21;
@@ -18,6 +20,8 @@ module.exports.RS_REQUEST_VALIDATION_FAILED = 90;
 module.exports.RS_ENTITY_NOT_FOUND = 101;
 module.exports.RS_ENTITY_ACCESS_FORBIDDEN = 102;
 module.exports.RS_ENTITY_DEPENDENCY_NOT_FOUND = 103;
+
+module.exports.RS_ERROR_GENERIC = 999;
 
 /* Generic definitions */
 module.exports.MILLISECONDS_PER_SECOND = 1000;
