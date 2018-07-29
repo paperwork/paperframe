93fb757052165f1c044bcbee7a54e6e481ddccdd
Marius <marius@twostairs.com>
2018-07-29 12:06:29 +0200

    Implemented EventEmitter into ServiceProvider

--- src/ServiceProvider.js
+++ src/ServiceProvider.js
@@ -1,14 +1,25 @@
 //@flow
 
+const EventEmitter = require('eventemitter2').EventEmitter2;
+const Base = require('./Base');
 
 import type {
     ControllerConfig
 } from './Controller';
 
-const Base = require('./Base');
 
 module.exports = class ServiceProvider extends Base {
+    _ee:                        EventEmitter
+
     initialize() {
         return true;
+    }
+
+    set ee(ee: EventEmitter) {
+        this._ee = ee;
+    }
+
+    get ee(): EventEmitter {
+        return this._ee;
     }
 };
